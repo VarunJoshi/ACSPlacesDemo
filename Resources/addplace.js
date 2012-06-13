d2ff24f18844a9444d60ebc2afafe3c2cab03281
Varun <vjoshi@appcelerator.com>
2012-06-13 09:54:30 -0700

    Added user

---
+++ Resources/addplace.js
@@ -0,0 +1,109 @@
+var Cloud = require('ti.cloud');
+Cloud.debug = true;
+
+var uiCreate = (function() {
+    var API = { };
+    API.name = "Create";
+    API.icon = "/KS_nav_ui.png";
+    API.parentNav = null;
+    API.win = null;
+
+    API.factoryView = function(opts) {
+
+        var content = Ti.UI.createScrollView({
+            top : 40,
+            contentHeight : 'auto',
+            layout : 'vertical'
+        });
+
+        var name = Ti.UI.createTextField({
+            hintText : 'Name',
+            top : 10,
+            left : 10,
+            right : 10,
+            height : 40,
+            borderStyle : Ti.UI.INPUT_BORDERSTYLE_ROUNDED
+        });
+        content.add(name);
+
+        var address = Ti.UI.createTextField({
+            hintText : 'Address',
+            top : 10,
+            left : 10,
+            right : 10,
+            height : 40,
+            borderStyle : Ti.UI.INPUT_BORDERSTYLE_ROUNDED
+        });
+        content.add(address);
+
+        var city = Ti.UI.createTextField({
+            hintText : 'City',
+            top : 10,
+            left : 10,
+            right : 10,
+            height : 40,
+            borderStyle : Ti.UI.INPUT_BORDERSTYLE_ROUNDED
+        });
+        content.add(city);
+
+        var state = Ti.UI.createTextField({
+            hintText : 'State',
+            top : 10,
+            left : 10,
+            right : 10,
+            height : 40,
+            borderStyle : Ti.UI.INPUT_BORDERSTYLE_ROUNDED
+        });
+        content.add(state);
+
+        var postalCode = Ti.UI.createTextField({
+            hintText : 'Postal Code',
+            top : 10,
+            left : 10,
+            right : 10,
+            height : 40,
+            borderStyle : Ti.UI.INPUT_BORDERSTYLE_ROUNDED,
+            keyboardType : Ti.UI.KEYBOARD_NUMBER_PAD
+        });
+        content.add(postalCode);
+
+        var createPlaceButton = Ti.UI.createButton({
+            title : 'Create',
+            top : 10,
+            left : 10,
+            right : 10,
+            bottom : 10,
+            height : 40
+        });
+        content.add(createPlaceButton);
+
+        function submitForm() {
+            //createPlaceButton.hide();
+
+            Cloud.Places.create({
+                name : name.value,
+                address : address.value,
+                city : city.value,
+                state : state.value,
+                postal_code : postalCode.value
+            }, function(e) {
+                if (e.success) {
+                    alert('Created!');
+                    name.value = address.value = city.value = state.value = postalCode.value = '';
+                } else {
+                    error(e);
+                }
+               // createPlaceButton.show();
+            });
+        }
+
+
+        createPlaceButton.addEventListener('click', submitForm);
+
+        return content;
+
+    };
+    return API;
+})();
+
+Ti.UI.currentWindow.add(uiCreate.factoryView({}));
---
+++ Resources/adduser.js
@@ -0,0 +1,120 @@
+var Cloud = require('ti.cloud');
+Cloud.debug = true;
+
+var uiCreateUser = (function() {
+    var API = { };
+    API.name = "Create User";
+    API.icon = "/KS_nav_ui.png";
+    API.parentNav = null;
+    API.win = null;
+
+    API.factoryView = function(opts) {
+
+        var content = Ti.UI.createScrollView({
+            top : 40,
+            contentHeight : 'auto',
+            layout : 'vertical'
+        });
+
+        var username = Ti.UI.createTextField({
+            hintText : 'Username',
+            top : 10,
+            left : 10,
+            right : 10,
+            height : 40,
+            borderStyle : Ti.UI.INPUT_BORDERSTYLE_ROUNDED,
+            autocapitalization : Ti.UI.TEXT_AUTOCAPITALIZATION_NONE,
+            autocorrect : false
+        });
+        content.add(username);
+
+        var password = Ti.UI.createTextField({
+            hintText : 'Password',
+            top : 10,
+            left : 10,
+            right : 10,
+            height : 40,
+            borderStyle : Ti.UI.INPUT_BORDERSTYLE_ROUNDED,
+            passwordMask : true
+        });
+        content.add(password);
+
+        var confirmPassword = Ti.UI.createTextField({
+            hintText : 'Confirm Password',
+            top : 10,
+            left : 10,
+            right : 10,
+            height : 40,
+            borderStyle : Ti.UI.INPUT_BORDERSTYLE_ROUNDED,
+            passwordMask : true
+        });
+        content.add(confirmPassword);
+
+        var firstName = Ti.UI.createTextField({
+            hintText : 'First Name',
+            top : 10,
+            left : 10,
+            right : 10,
+            height : 40,
+            borderStyle : Ti.UI.INPUT_BORDERSTYLE_ROUNDED
+        });
+        content.add(firstName);
+
+        var lastName = Ti.UI.createTextField({
+            hintText : 'Last Name',
+            top : 10,
+            left : 10,
+            right : 10,
+            height : 40,
+            borderStyle : Ti.UI.INPUT_BORDERSTYLE_ROUNDED
+        });
+        content.add(lastName);
+
+        var button = Ti.UI.createButton({
+            title : 'Create',
+            top : 10,
+            left : 10,
+            right : 10,
+            bottom : 10,
+            height : 40
+        });
+        content.add(button);
+
+        function submitForm() {
+
+            if (password.value != confirmPassword.value) {
+                alert('Passwords do not match!');
+                confirmPassword.focus();
+                return;
+            }
+            //button.hide();
+
+            Cloud.Users.create({
+                username : username.value,
+                password : password.value,
+                password_confirmation : confirmPassword.value,
+                first_name : firstName.value,
+                last_name : lastName.value
+            }, function(e) {
+                if (e.success) {
+                    var user = e.users[0];
+                    alert('Created! You are now logged in as ' + user.id);
+                    username.value = password.value = confirmPassword.value = firstName.value = lastName.value = '';
+                } else {
+                    error(e);
+                }
+                //button.show();
+            });
+        }
+
+
+        button.addEventListener('click', submitForm);
+
+        return content;
+
+    };
+
+    return API;
+})();
+
+Ti.UI.currentWindow.add(uiCreateUser.factoryView({}));
